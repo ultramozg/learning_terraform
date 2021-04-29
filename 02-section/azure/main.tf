646e110c84f8fdd8b83bddf34140176d52e99c58
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-29 14:30:28 +0300

    add vm , update resource group name

--- 02-section/azure/main.tf
+++ 02-section/azure/main.tf
@@ -79,7 +79,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 resource "azurerm_network_interface" "myterraformnic" {
     name                        = "myNIC"
     location                    = "eastus"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    resource_group_name         = azurerm_resource_group.myresourcegroup.name
 
     ip_configuration {
         name                          = "myNicConfiguration"
@@ -97,4 +97,51 @@ resource "azurerm_network_interface" "myterraformnic" {
 resource "azurerm_network_interface_security_group_association" "example" {
     network_interface_id      = azurerm_network_interface.myterraformnic.id
     network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+resource "azurerm_ssh_public_key" "mysshpubkey" {
+  name                = "mysshpubkey"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  location            = "West Europe"
+  public_key          = file("~/.ssh/id_rsa.pub")
+}
+
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "West Europe"
+    resource_group_name   = azurerm_resource_group.myresourcegroup.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "myvm"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username       = "azureuser"
+        public_key     = azurerm_ssh_public_key.mysshpubkey.public_key
+    }
+
+    /*
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+    */
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
