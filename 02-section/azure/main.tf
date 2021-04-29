f8511aed4751270568fb2346f242faefda5cb009
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-29 14:14:36 +0300

    add network security group

--- 02-section/azure/main.tf
+++ 02-section/azure/main.tf
@@ -53,3 +53,25 @@ resource "azurerm_public_ip" "myterraformpublicip" {
         environment = "Terraform Demo"
     }
 }
+
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "Wast Europe"
+    resource_group_name = azurerm_resource_group.myresourcegroup.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
