7f7e6aca44b707df57e27f0c13a3827551d58d0e
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-29 15:57:47 +0300

    move hardcoded credentials to vars

--- 02-section/azure/main.tf
+++ 02-section/azure/main.tf
@@ -10,9 +10,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "02b20795-af5d-43bf-b6b6-e9f4e231a788"
-  client_id       = "c3efce2e-281e-49a1-a37c-4a73b53086d0"
-  tenant_id       = "eac6c7bf-5c15-44bb-aec8-7a9c752e9876"
+  subscription_id = var.credentials["subscription_id"]
+  client_id       = var.credentials["client_id"]
+  tenant_id       = var.credentials["tenant_id"]
 }
 
 terraform {
--- 02-section/azure/vars.tf
+++ 02-section/azure/vars.tf
@@ -1,4 +1,11 @@
-variable "client_secret" {} 
+variable "credentials" {
+    type = map
+    default = {
+      subscription_id = "02b20795-af5d-43bf-b6b6-e9f4e231a788"
+      client_id = "c3efce2e-281e-49a1-a37c-4a73b53086d0"
+      tenant_id = "eac6c7bf-5c15-44bb-aec8-7a9c752e9876"
+    }
+}
 
 variable "location" {
     default = "westeurope"
