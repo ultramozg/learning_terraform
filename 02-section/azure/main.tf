0c32a2d3df98a1f68b850030b1e5f7eb44f575a8
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-29 14:20:59 +0300

    add network interface and network_interface_security_group_name_association

--- 02-section/azure/main.tf
+++ 02-section/azure/main.tf
@@ -75,3 +75,26 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         environment = "Terraform Demo"
     }
 }
+
+resource "azurerm_network_interface" "myterraformnic" {
+    name                        = "myNIC"
+    location                    = "eastus"
+    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.myterraformnic.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
