f8d6750699126c00d6bf80012fb73db936303545
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-30 16:06:54 +0300

    update elb, healthcheck to use 80

--- 03-section/aws/elb.tf
+++ 03-section/aws/elb.tf
@@ -3,7 +3,7 @@ resource "aws_elb" "my-elb" {
   subnets         = [aws_subnet.main-public.id]
   security_groups = [aws_security_group.elb-securitygroup.id]
   listener {
-    instance_port     = 8080
+    instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
@@ -12,7 +12,7 @@ resource "aws_elb" "my-elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:8080/"
+    target              = "HTTP:80/"
     interval            = 30
   }
 
