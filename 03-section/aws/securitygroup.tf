03a33938ee9c731a204002011f216b9d80a1b758
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-30 15:15:37 +0300

    return back port range

--- 03-section/aws/securitygroup.tf
+++ 03-section/aws/securitygroup.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "elb-securitygroup" {
 
   ingress {
     from_port   = 80
-    to_port     = 8080
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
