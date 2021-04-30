4938ca86b1c542edc9d09456b1a9341edd8367cf
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-30 09:35:13 +0300

    change aws backend for section 3

--- 03-section/aws/config.tf
+++ 03-section/aws/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-01"
-    key    = "learning_terraform/terraform.tfstate"
+    key    = "learning_terraform/terraform-03section.tfstate"
     region = "us-east-2"
   }
 }
