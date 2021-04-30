dcffc97e2be93a8bf5f4df1c69c01df1ea136e11
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-30 16:26:27 +0300

    allow connecto only lb to backend servers

--- 03-section/aws/securitygroup.tf
+++ 03-section/aws/securitygroup.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "allow-http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.elb-securitygroup.id]
   }
   tags = {
     Name = "allow-http"
