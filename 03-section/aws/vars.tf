edb4959cf48c8b2c6572bd63ba35129ab6edf1c8
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-30 09:58:48 +0300

    query aws to get ami in this zone

--- 03-section/aws/vars.tf
+++ 03-section/aws/vars.tf
@@ -17,3 +17,11 @@ variable "AMIS" {
   }
 }
 
+data "aws_ami_ids" "ubuntu" {
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-amd64-minimal-*"]
+  }
+}
