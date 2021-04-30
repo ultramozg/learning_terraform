488f9f9708f7242cf5b07713b12cdeb148b93777
dmitri.candu <dmitri.candu@amdaris.com>
2021-04-30 10:32:00 +0300

    move out ami name to custom variable

--- 03-section/aws/vars.tf
+++ 03-section/aws/vars.tf
@@ -10,11 +10,15 @@ variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
 
+variable "AMI_NAME" {
+  default = "ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-amd64-minimal-*"
+}
+
 data "aws_ami_ids" "ubuntu" {
   owners = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-amd64-minimal-*"]
+    values = [var.AMI_NAME]
   }
 }
