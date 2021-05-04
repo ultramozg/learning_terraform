9cfba8b0865aa55652e677b8a97d4c257c1d280a
dmitri.candu <dmitri.candu@amdaris.com>
2021-05-04 10:27:53 +0300

    add nat gateway ,  route table to that nat resource

--- 03-section/aws/vpc.tf
+++ 03-section/aws/vpc.tf
@@ -42,6 +42,18 @@ resource "aws_internet_gateway" "main-gw" {
   }
 }
 
+# nat gw
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.main-public.id
+  depends_on    = [aws_internet_gateway.main-gw]
+}
+
+
 # route tables
 resource "aws_route_table" "main-public" {
   vpc_id = aws_vpc.main.id
@@ -55,9 +67,26 @@ resource "aws_route_table" "main-public" {
   }
 }
 
+resource "aws_route_table" "main-private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
+  }
+
+  tags = {
+    Name = "main-private"
+  }
+}
+
+
 # route associations public
-resource "aws_route_table_association" "main-public-a" {
+resource "aws_route_table_association" "main-public" {
   subnet_id      = aws_subnet.main-public.id
   route_table_id = aws_route_table.main-public.id
 }
 
+resource "aws_route_table_association" "main-private" {
+  subnet_id      = aws_subnet.main-private.id
+  route_table_id = aws_route_table.main-private.id
+}
