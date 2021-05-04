d30adc4bd44df378959c18f01f19c93c886458a9
dmitri.candu <dmitri.candu@amdaris.com>
2021-05-04 11:36:58 +0300

    add application_port admin_user admin_password variables

--- 03-section/azure/vars.tf
+++ 03-section/azure/vars.tf
@@ -24,3 +24,17 @@ variable "resource_group_name" {
  description = "The name of the resource group in which the resources will be created"
  default     = "myResourceGroup"
 }
+
+variable "application_port" {
+   description = "The port that you want to expose to the external load balancer"
+   default     = 80
+}
+
+variable "admin_user" {
+   description = "User name to use as the admin account on the VMs that will be part of the VM Scale Set"
+   default     = "azureuser"
+}
+
+variable "admin_password" {
+   description = "Default password for admin account"
+}
