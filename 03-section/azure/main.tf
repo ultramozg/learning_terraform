91795e326f67885cc2701240319bfe11f21007ac
dmitri.candu <dmitri.candu@amdaris.com>
2021-05-04 10:54:02 +0300

    change port 8080 -> 80

--- 03-section/azure/main.tf
+++ 03-section/azure/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "8080"
+        destination_port_range     = "80"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
