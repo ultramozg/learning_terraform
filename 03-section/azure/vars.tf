b582ab7806ce22d5c1beed2e50bcd66aa8f4ae5b
dmitri.candu <dmitri.candu@amdaris.com>
2021-05-04 11:21:55 +0300

    add tags and resource_group_name variables

--- 03-section/azure/vars.tf
+++ 03-section/azure/vars.tf
@@ -10,3 +10,17 @@ variable "credentials" {
 variable "location" {
     default = "westeurope"
 }
+
+variable "tags" {
+ description = "A map of the tags to use for the resources that are deployed"
+ type        = map(string)
+
+ default = {
+   environment = "codelab"
+ }
+}
+
+variable "resource_group_name" {
+ description = "The name of the resource group in which the resources will be created"
+ default     = "myResourceGroup"
+}
