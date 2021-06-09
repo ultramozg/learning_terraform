6ef83c371d5ed02c4020584d4169317eb3a8f397
dmitri.candu <dmitri.candu@amdaris.com>
2021-06-09 17:41:11 +0300

    add new section 05 which contains information about k8s

---
+++ 05-section/azure/main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "test-aks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
---
+++ 05-section/azure/variable.tf
@@ -0,0 +1,11 @@
+variable resource_group_name {
+  default = "dev-aks-rg"
+}
+
+variable location {
+  default = "ukwest"
+}
+
+variable environment {
+    default = "dev"
+}
