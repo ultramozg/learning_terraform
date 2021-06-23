b7ea343fa269e83413acb7ea0c5c5c7cc104ed0a
dmitri.candu <dmitri.candu@amdaris.com>
2021-06-23 10:10:53 +0300

    add nat_gateway

---
+++ 05-section/azure/nat.tf
@@ -0,0 +1,27 @@
+resource "azurerm_public_ip" "pubip" {
+  name                = "nat-gateway-publicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
+}
+
+resource "azurerm_nat_gateway" "nat_gateway" {
+  name                    = "nat-Gateway"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  zones                   = ["1"]
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "association" {
+  nat_gateway_id       = azurerm_nat_gateway.nat_gateway.id
+  public_ip_address_id = azurerm_public_ip.pubip.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "association" {
+  subnet_id      = azurerm_subnet.frontend.id
+  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
+}
