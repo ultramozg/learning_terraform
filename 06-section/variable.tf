9ce773560cc415fe6dd163db687a1045229c6ec2
dmitri.candu <dmitri.candu@amdaris.com>
2021-06-25 12:26:27 +0300

    formatting

--- 06-section/variable.tf
+++ 06-section/variable.tf
@@ -1,13 +1,13 @@
 variable "k8s" {
   description = "This variable holds information about the kubernetes configuration"
-  type        = object({
-    name = string
-    rg = string
-    environment = string
+  type = object({
+    name               = string
+    rg                 = string
+    environment        = string
     docker_bridge_cidr = string
-    dns_service_ip = string
-    service_cidr = string
-    vnet = map(any)
+    dns_service_ip     = string
+    service_cidr       = string
+    vnet               = map(any)
   })
   default = {
     name               = "ssh-aks-nonprod"
@@ -17,7 +17,7 @@ variable "k8s" {
     dns_service_ip     = "10.65.0.10"
     service_cidr       = "10.65.0.0/16"
     vnet = {
-      address_space = ["10.64.0.0/12"]
+      address_space    = ["10.64.0.0/12"]
       address_prefixes = ["10.64.0.0/16"]
     }
   }
@@ -25,7 +25,7 @@ variable "k8s" {
 
 variable "logs_analytics_workspace" {
   default = {
-    name = "DefaultWorkspace-0a277bdd-294c-4772-b5b0-2e57623e7175-SUK"
+    name    = "DefaultWorkspace-0a277bdd-294c-4772-b5b0-2e57623e7175-SUK"
     rg_name = "defaultresourcegroup-suk"
   }
 }
