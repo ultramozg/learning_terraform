1a93af579ced9ade20aa6b47758f5fbcdfda115e
dmitri.candu <dmitri.candu@amdaris.com>
2021-07-22 14:27:22 +0300

    add initial work for the service bus

---
+++ 07-section/variable.tf
@@ -0,0 +1,14 @@
+variable rg_name {
+  type = string
+  default = "test-rg-name"
+}
+
+variable location {
+  type = string
+  default = "uksouth"
+}
+
+variable environment {
+  type = string
+  default = "dev"
+}
