711fa14bae8f453f67d2eb2b75d0d61c1f34ff7e
dmitri.candu <dmitri.candu@amdaris.com>
2021-07-22 14:56:10 +0300

    add temporary variable

--- 07-section/variable.tf
+++ 07-section/variable.tf
@@ -12,3 +12,21 @@ variable environment {
   type = string
   default = "dev"
 }
+
+variable service_bus {
+  description = "this variable hold all information about service bus itself and queues"
+  type = object({
+    name = string
+    queues = map(any)
+  })
+
+  default = {
+    name = "test-service-bus-dev"
+    queues = {
+      fist-queue = {
+        "param1" = "pampam"
+        "param2" = "pampam"
+      }
+    }
+  }
+}
