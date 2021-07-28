d7a8eb080d44e18d401916dfd7d1e2b0d8b4ef9b
dmitri.candu <dmitri.candu@amdaris.com>
2021-07-28 11:27:13 +0300

    set the service type to cluster ip , later on it can be changed

--- 05-section/azure/helm.tf
+++ 05-section/azure/helm.tf
@@ -5,25 +5,9 @@ resource "helm_release" "chaos-mesh" {
   namespace        = "chaos-testing"
   create_namespace = true
   version          = "2.0.0"
-  /*
-  values = [
-    "${file("values.yaml")}"
-  ]
 
   set {
-    name  = "cluster.enabled"
-    value = "true"
+    name  = "service.type"
+    value = "ClusterIP"
   }
-
-  set {
-    name  = "metrics.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "service.annotations.prometheus\\.io/port"
-    value = "9127"
-    type  = "string"
-  }
-  */
 }
