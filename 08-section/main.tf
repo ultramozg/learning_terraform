d990e7a93291594a668b795d7d1ac882f679b7b7
dmitri.candu <dmitri.candu@amdaris.com>
2021-07-28 16:38:41 +0300

    use service bus module; add resource group creation

---
+++ 08-section/main.tf
@@ -0,0 +1,18 @@
+module "service_bus" {
+  source      = "service_bus"
+
+  rg_name     = "dev-rg"
+  environment = "dev"
+
+  service_bus = {
+    name = "dev-service-bus"
+    queues = {
+      test-queue = {
+        max_size = 1024
+        lock_duration = "PT1M"
+        max_delivery_count = 10
+        enable_partitioning = false
+      }
+    }
+  }
+}
