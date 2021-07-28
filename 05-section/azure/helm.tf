5636d19b41fb91289df2e3cacaf7881347f6590f
dmitri.candu <dmitri.candu@amdaris.com>
2021-07-28 11:19:01 +0300

    add helm provider, deploy the chaos-mesh helm chart

---
+++ 05-section/azure/helm.tf
@@ -0,0 +1,29 @@
+resource "helm_release" "chaos-mesh" {
+  name             = "chaos-mesh"
+  repository       = "https://charts.chaos-mesh.org"
+  chart            = "chaos-mesh"
+  namespace        = "chaos-testing"
+  create_namespace = true
+  //version    = "6.0.1"
+  /*
+  values = [
+    "${file("values.yaml")}"
+  ]
+
+  set {
+    name  = "cluster.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "metrics.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "service.annotations.prometheus\\.io/port"
+    value = "9127"
+    type  = "string"
+  }
+  */
+}
