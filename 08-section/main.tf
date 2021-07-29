d1d1dad48f6ef4543b902bc255aaf1c94eb4575e
dmitri.candu <dmitri.candu@amdaris.com>
2021-07-29 11:07:13 +0300

    remove unused value

--- 08-section/main.tf
+++ 08-section/main.tf
@@ -4,13 +4,13 @@ module "service_bus" {
   rg_name     = "dev-rg"
   environment = "dev"
 
+  // Geo-Recovery only works with Premium sku
   geo_recovery = {
     enabled  = true
     // When we choose the Premium sku plan by default it 1,
     // but we should provide this value anyway.
     capacity = 1
   }
-  sku = "Premium"
 
   service_bus = {
     name = "ssh-dev-service-bus"
